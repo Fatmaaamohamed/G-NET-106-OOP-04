323156b83e3bf0bcf350e33684c031e910243193
Fatma_Mohamed <fm5252498@gmail.com>
2026-09-23 00:45:22 +0300

    Question06

---
+++ G-NET-106-OOP-04/DeliveryReport.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G_NET_106_OOP_04
+{
+    internal class DeliveryReport
+    {
+        #region Question06
+        public static void PrintShipment(ITrackable shipment)
+        {
+            if (shipment != null)
+            {
+                Console.WriteLine($"{shipment.GetTrackingStatus()}");
+            }
+        }
+
+        public static void PrintInsurance(IInsurable shipment)
+        {
+            if (shipment != null)
+            {
+                Console.WriteLine($"Insurance Cost: {shipment.CalculateInsurance()} EGP");
+            }
+        }
+
+
+        #endregion
+    }
+}
