a403574fb2b531552bc7cb0fcd059abe690cb3b8
Fatma_Mohamed <fm5252498@gmail.com>
2026-09-22 23:17:44 +0300

    Question01

--- G-NET-106-OOP-04/Program.cs
+++ G-NET-106-OOP-04/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
+            #region Question01
 
+            /*
+             
+             a) Abstraction hides complex implementation details and exposes only essential features.
+                It focuses on what an object does rather than how it does it .
+             
+             b) Reduces Complexity: Shields developers from unnecessary code details.
+                Enhances Maintainability: Internal implementation can change without breaking dependent code.
+                Security: Prevents direct access to internal state and logic.
+             
+             */
+
+            #endregion
         }
     }
 }
