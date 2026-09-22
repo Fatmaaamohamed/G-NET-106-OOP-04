0550c7958e613587843a1ac73fc23e5b3bc985a1
Fatma_Mohamed <fm5252498@gmail.com>
2026-09-23 00:51:56 +0300

    Question07

---
+++ G-NET-106-OOP-04/Driver.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G_NET_106_OOP_04
+{
+    internal class Driver
+    {
+        public string DriverName { get; set; } = string.Empty;
+    }
+}
